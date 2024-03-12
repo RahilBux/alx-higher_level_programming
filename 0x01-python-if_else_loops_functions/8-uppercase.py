e097e462af9d660580572368149f24240d76b1b4
RahilBux <rahilbux@gmail.com>
2024-03-12 14:50:22 +0200

    added task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-def islower(c):
-    if ord(c) >= ord('a') and ord(c) <= ord('z'):
-        return True
-    else:
-        return False
-
-
 def uppercase(str):
     convert = ord('a') - ord('A')
     for i in str:
-        print("{:c}".format(ord(i) if not islower(i) else ord(i) - 32), end="")
+        print("{:c}".format(ord(i) - convert if ord(i) >= ord('a')
+                            and ord(i) <= ord('z') else ord(i)), end="")
     print("")
