fca6ef59444c6ad4b75dd56ce61dc68fd209846d
RahilBux <rahilbux@gmail.com>
2024-03-16 14:08:38 +0200

    fixed task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@ def print_matrix_integer(matrix=[[]]):
     for i in range(len1):
         len2 = len(matrix[i])
         for j in range(len2):
-            print("{}".format(matrix[i][j]), end=" " if j != len2 - 1 else "")
+            print('{}'.format(matrix[i][j]), end=" " if j != len2 - 1 else "")
         print("")
