cc8cb230150da30b423f9de167eb65d1c073379f
RahilBux <rahilbux@gmail.com>
2024-03-18 15:27:53 +0200

    fixed

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,45 +1,113 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * is_palindrome - checks if a linked list is a palindrome
- * @head: beginning of list
+ * rev_listint - reverses a linked list
+ * @head: pointer to first node of list
+ *
+ */
+
+void rev_listint(listint_t **head)
+{
+	listint_t *next = NULL;
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	*head = prev;
+}
+
+/**
+ * create_node - creates a node
+ * @data: integer to insert to new node
+ *
+ * Return: pointer to new node
+ */
+
+listint_t *create_node(int data)
+{
+	listint_t *newnode = malloc(sizeof(listint_t));
+	if (newnode == NULL)
+	{
+		printf("Error allocating memory\n");
+		exit(1);
+	}
+	newnode->n = data;
+	newnode->next = NULL;
+	return (newnode);
+}
+
+/**
+ * dup_list - duplicates a singly linked list
+ * @original: pointer to the original list
+ *
+ * Return: pointer to the duplicate list
+ */
+
+listint_t *dup_list(listint_t *original)
+{
+	if (original == NULL)
+		return (NULL);
+
+	listint_t *newhead = create_node(original->n);
+
+	listint_t *curroriginal = original->next;
+	listint_t *currnew = newhead;
+
+	while (curroriginal != NULL)
+	{
+		currnew->next = create_node(curroriginal->n);
+		curroriginal = curroriginal->next;
+		currnew = currnew->next;
+	}
+	return (newhead);
+}
+
+/**
+ * free_list - frees a linked list
+ * @head: pointer to the first node
+ */
+
+void free_list(listint_t *head)
+{
+	listint_t *temp;
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
+
+/**
+ * is_palindrome - checks if linked list is a palindrome
+ * @head: pointer to first node
  *
  * Return: 0 if not a palindrome and 1 if it is
  */
 
 int is_palindrome(listint_t **head)
 {
-	if (*head == NULL || *(head)->next == NULL)
+	if (*head == NULL)
 		return (1);
-
-	int i = 0;
-	int sum1 = 0, sum2 = 0;
-	listint_t *p1 = *head;
-	listint_t *p2 = *head;
+	listint_t *dup = dup_list(*head), *p1 = *head;
+	rev_listint(&dup);
+	listint_t *p2 = dup;
 
 	while (p1 != NULL)
 	{
-		i++;
-		p1 = p1->next;
-	}
-	p1 = *head;
-	if (i % 2 != 0)
-		return (0);
-	i = i / 2;
-	while (i > 0)
-	{
-		p1 = p1->next;
-		i--;
-	}
-
-	while (p1 != NULL)
-	{
-		sum1 = sum1 + p1->n;
-		sum2 = sum2 + p2->n;
+		if (p1->n != p2->n)
+			return (0);
 		p1 = p1->next;
 		p2 = p2->next;
 	}
-	if (sum1 == sum2)
-		return (1);
-	return (0);
+	free(dup);
+	return (1);
 }
