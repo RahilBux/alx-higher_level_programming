1065f94a176339b01117fe0c7fb2859b459ad845
RahilBux <rahilbux@gmail.com>
2024-03-18 15:41:12 +0200

    Fixed

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,17 +1,14 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
  * rev_listint - reverses a linked list
- * @head: pointer to first node of list
- *
+ * @head: pointer to first node
  */
 
 void rev_listint(listint_t **head)
 {
-	listint_t *next = NULL;
 	listint_t *prev = NULL;
+	listint_t *next = NULL;
 	listint_t *current = *head;
 
 	while (current != NULL)
@@ -25,92 +22,48 @@ void rev_listint(listint_t **head)
 }
 
 /**
- * create_node - creates a node
- * @data: integer to insert to new node
- *
- * Return: pointer to new node
- */
-
-listint_t *create_node(int data)
-{
-	listint_t *newnode = malloc(sizeof(listint_t));
-
-	if (newnode == NULL)
-	{
-		printf("Error allocating memory\n");
-		exit(1);
-	}
-	newnode->n = data;
-	newnode->next = NULL;
-	return (newnode);
-}
-
-/**
- * dup_list - duplicates a singly linked list
- * @original: pointer to the original list
- *
- * Return: pointer to the duplicate list
- */
-
-listint_t *dup_list(listint_t *original)
-{
-	if (original == NULL)
-		return (NULL);
-
-	listint_t *newhead = create_node(original->n);
-
-	listint_t *curroriginal = original->next;
-	listint_t *currnew = newhead;
-
-	while (curroriginal != NULL)
-	{
-		currnew->next = create_node(curroriginal->n);
-		curroriginal = curroriginal->next;
-		currnew = currnew->next;
-	}
-	return (newhead);
-}
-
-/**
- * free_list - frees a linked list
- * @head: pointer to the first node
- */
-
-void free_list(listint_t *head)
-{
-	listint_t *temp;
-
-	while (head != NULL)
-	{
-		temp = head;
-		head = head->next;
-		free(temp);
-	}
-}
-
-/**
  * is_palindrome - checks if linked list is a palindrome
- * @head: pointer to first node
+ * @head: pointer to head
  *
- * Return: 0 if not a palindrome and 1 if it is
+ * Return: 1 if its a palindrome and 0 if not
  */
 
 int is_palindrome(listint_t **head)
 {
+	listint_t *s = *head, *f = *head, *temp = *head, *dup = NULL;
+
 	if (*head == NULL)
 		return (1);
-	listint_t *dup = dup_list(*head), *p1 = *head;
 
-	rev_listint(&dup);
-	listint_t *p2 = dup;
-
-	while (p1 != NULL)
+	while (1)
 	{
-		if (p1->n != p2->n)
-			return (0);
-		p1 = p1->next;
-		p2 = p2->next;
+		f = f->next->next;
+		if (f == NULL)
+		{
+			dup = s->next;
+			break;
+		}
+		if (f->next == NULL)
+		{
+			dup = s->next->next;
+			break;
+		}
+		s = s->next;
 	}
-	free(dup);
-	return (1);
+	rev_listint(&dup);
+
+	while (dup != NULL && temp != NULL)
+	{
+		if (temp->n == dup->n)
+		{
+			dup = dup->next;
+			temp = temp->next;
+		}
+		else
+			return (0);
+	}
+	if (dup == NULL)
+		return (1);
+
+	return (0);
 }
