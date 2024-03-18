f1dfe2675cfbdce6155414cbd8843690483c3140
RahilBux <rahilbux@gmail.com>
2024-03-18 15:43:39 +0200

    tried

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -32,7 +32,7 @@ int is_palindrome(listint_t **head)
 {
 	listint_t *s = *head, *f = *head, *temp = *head, *dup = NULL;
 
-	if (*head == NULL)
+	if (*head == NULL || *(head)->next == NULL)
 		return (1);
 
 	while (1)
