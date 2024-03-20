952ff62c0b9f851575c05d48e7197752f29f5ec1
RahilBux <rahilbux@gmail.com>
2024-03-20 18:54:26 +0200

    fixed

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,8 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
+    elif len(a_dictionary) == 0:
+        return None
     result = 0
     name = ""
     for key, value in a_dictionary.items():
