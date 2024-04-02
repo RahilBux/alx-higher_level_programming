23771fc3092b1906e8a895b333bc3a45cc0a7ac3
RahilBux <rahilbux@gmail.com>
2024-04-03 00:45:38 +0200

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module for square"""
+
+
+class Square:
+    """A square"""
+
+
+    def __init__(s, size):
+        """Construct
+        Args:
+            size: length of side
+        """
+        s.__size = size
