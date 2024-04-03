70d670137ae1bbd22c12a888386de86f32dbcf7c
RahilBux <rahilbux@gmail.com>
2024-04-03 02:05:16 +0200

    task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Def square"""
+
+
+class Square:
+    """Rep Square"""
+
+    def __init__(s, size=0):
+        """New square
+        Args:
+            size(int): Size of side
+        """
+        s.size = size
+
+    @property
+    def size(s):
+        """get size of square"""
+        return (s.__size)
+
+    @size.setter
+    def size(s, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        s.__size = value
+
+    def area(s):
+        "return the area"
+        return (s.__size * s.__size)
+
+    def my_print(self):
+        """Print the square with # char"""
+        for i in range(0, self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print("")
+        if self.__size == 0:
+            print("")
