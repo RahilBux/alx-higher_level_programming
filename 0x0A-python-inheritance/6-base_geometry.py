de15a0cc9218a24cd9db179db80d2f39620f979b
RahilBux <rahilbux@gmail.com>
2024-04-15 16:47:56 +0200

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module for Class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Initialize class BaseGeometry"""
+    def area(self):
+        """Returns area"""
+        raise Exception("area() is not implemented")
