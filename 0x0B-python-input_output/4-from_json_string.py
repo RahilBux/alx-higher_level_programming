79b3340807d8ea347e3705b128b713c42b100ceb
RahilBux <rahilbux@gmail.com>
2024-04-16 16:36:08 +0200

    task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module for function from_json_string"""
+import json
+
+
+def my_json_string(my_str):
+    """Function that returns an object represented by JSON string
+    args:
+        my_str: object
+    """
+    return json.loads(my_str)
