b5224cf6467909ec596a24d513825103692206e8
RahilBux <rahilbux@gmail.com>
2024-04-16 15:43:13 +0200

    taks 9

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,5 @@ def read_file(filename=""):
     Args:
         filename: name of file in string
     """
-    with open(filename, "r", encoding="utf-8") as f:
-        print(f.read())
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
