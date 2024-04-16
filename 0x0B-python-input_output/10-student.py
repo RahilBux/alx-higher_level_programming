7bb37777b449152d358235e437321a3f01fa8120
RahilBux <rahilbux@gmail.com>
2024-04-17 01:59:32 +0200

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Definition of Student class"""
+
+
+class Student:
+    """Student class object"""
+    def __init__(self, first_name, last_name, age):
+        """Initialize class
+        Args:
+            first_name: first name of student
+            last_name: last name of student
+            age: age of student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Gets the dictionary rep of the Student instance
+        Args:
+            attrs: the attributes
+        """
+        if (type(attrs) is list and all(type(i) is str for i in attrs)):
+            return {j: getattr(self, j) for j in attrs if hasattr(self, j)}
+        return self.__dict__
