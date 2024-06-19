b5a617a583247f305695db457be7321031199b5a
RahilBux <rahilbux@gmail.com>
2024-06-19 02:06:57 +0200

    task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+if (process.argv[3] === undefined) {
+  console.log(0);
+} else {
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
+}
