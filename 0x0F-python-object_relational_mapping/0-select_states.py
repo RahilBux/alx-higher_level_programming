33ffc08b82d9061bad1f5859062994e22eb37860
RahilBux <rahilbux@gmail.com>
2024-06-23 17:28:38 +0200

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Lists the states of USA"""
+"""Lists states"""
+
 import MySQLdb
 from sys import argv
 
-
 if __name__ == "__main__":
-    con = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                          passwd=argv[2], db=argv[3], charset="utf-8")
-    curr = con.cursor()
-    curr.execute("SELECT * FROM states ORDER BY states.id ASC")
-    query_row = curr.fetchall()
-    for row in query_row:
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
-    curr.close()
-    con.close()
+    cur.close()
+    conn.close()
