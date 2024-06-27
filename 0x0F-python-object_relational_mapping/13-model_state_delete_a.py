482a408498648c97c5d5dfa8ce597b5f197a4b27
RahilBux <rahilbux@gmail.com>
2024-06-27 19:51:03 +0000

    task 13 pycode

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     eng = create_engine(
-            'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
         .format(argv[1], argv[2],
                 argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(eng)
