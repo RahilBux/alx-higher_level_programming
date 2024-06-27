f39c0e06145455b1c700346e2e2fa26c60954df6
RahilBux <rahilbux@gmail.com>
2024-06-27 19:44:49 +0000

    task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Task 7 of state"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    eng = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2],
+                argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    new_st = State(name="Louisiana")
+    session.add(new_st)
+    session.commit()
+    print(new_st.id)
+    session.close()
