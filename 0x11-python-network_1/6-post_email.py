b61cd1e8c0dd1b0df72ed957dd854a5069faa527
RahilBux <rahilbux@gmail.com>
2024-07-06 02:11:29 +0200

    task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Script that takes URL and posts Email
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    read = {"email": argv[2]}
+    r = requests.post(argv[1], data=read)
+    print(t.text)
